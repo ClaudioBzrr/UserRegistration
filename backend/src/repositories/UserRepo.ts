54be221861a404e955aa903d9c8ce9133ceecb72
claudiobzrr <claudio.bezerra1998@gmail.com>
2023-03-30 21:45:26 -0300

    templated new technologies and remove older code

--- backend/src/repositories/UserRepo.ts
+++
@@ -1,9 +0,0 @@
-import { Repository, EntityRepository } from "typeorm";
-import { User } from "../entities/User";
-
-@EntityRepository(User)
-class UserRepo extends Repository<User>{
-
-}
-
-export {UserRepo}
--- frontend/src/auth/Authenticated.ts
+++
@@ -1,8 +0,0 @@
-export  function IsAuthenticated(){
-
-    if(localStorage.getItem('userId')){
-        return true
-    }else{
-        return false
-    }
-}
