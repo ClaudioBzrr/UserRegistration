3c6f077578b603f3f3d94c865ece8d1d13e96744
claudiobzrr <39713988+ClaudioBzrr@users.noreply.github.com>
2023-03-31 16:20:56 +0000

    added cors in server file

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,14 @@
 import express from 'express'
 import 'dotenv/config'
+import cors from 'cors'
 
-const server = express()
+
 const port = String(process.env.SERVER_PORT)
+const server = express()
+
+server.use(cors())
+server.use(express.json({limit:'2mb'}))
+
 server.listen(port || process.env.PORT,() =>{
     console.log(`Server listening on port ${port} or ${process.env.PORT} ...`)
 })
