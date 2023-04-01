58347f1f5b0b36b6ad558581c9056eed072d251f
claudiobzrr <claudio.bezerra1998@gmail.com>
2023-04-01 08:39:26 -0300

    added sendmail with credentials function

--- backend/src/use-case/create-customer-user-case.ts
+++ backend/src/use-case/create-customer-user-case.ts
@@ -1,3 +1,4 @@
+import { MailAdapter } from "../adapters/mail-adapter";
 import { CustomerRepository } from "../repositories/customer-repository";
 import { EncryptRepository } from "../repositories/encrypt-repository";
 
@@ -9,12 +10,23 @@ interface CreateCustomerUseCaseData{
 export class CreateCustomerUseCase{
     constructor(
         private customerRepository:CustomerRepository,
-        private encryptRepository:EncryptRepository
+        private encryptRepository:EncryptRepository,
+        private mailAdapter:MailAdapter
     ){}
 
     async execute({email,name}:CreateCustomerUseCaseData){
         const rawPassword = Math.random().toString(36).slice(-8)
         const password =  await this.encryptRepository.hash(rawPassword)
         await this.customerRepository.create({email,name,password})
+        await this.mailAdapter.sendMail({
+            recipient:email,
+            subject:'New customer registered',
+            body:[
+                `<p>Hi!</p>`,
+                `<p>Below are your platform access credentials</p>`,
+                `<p><b>Login : ${email}</b></p>`,
+                `<p><b>Password : ${password}</b></p>`
+            ].join('\n')
+        })
     }
 }
