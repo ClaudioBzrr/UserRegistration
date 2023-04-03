8d9c621c703fb494ebabfd209159b6c3ad850730
claudiobzrr <claudio.bezerra1998@gmail.com>
2023-04-02 22:34:20 -0300

    added routes import in server file

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import 'dotenv/config'
 import cors from 'cors'
+import { routes } from './routes'
 
 
 const port = String(process.env.SERVER_PORT)
@@ -8,6 +9,7 @@ const server = express()
 
 server.use(cors())
 server.use(express.json({limit:'2mb'}))
+server.use(routes)
 
 server.listen(port || process.env.PORT,() =>{
     console.log(`Server listening on port ${port} or ${process.env.PORT} ...`)
