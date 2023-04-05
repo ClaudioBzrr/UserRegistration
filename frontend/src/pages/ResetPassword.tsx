8e6a46cf2eb4a6a0f3231d0da7054d1ad993da3d
claudiobzrr <39713988+ClaudioBzrr@users.noreply.github.com>
2023-04-05 16:46:53 +0000

    Created reset password page style

--- frontend/src/pages/ResetPassword.tsx
+++ frontend/src/pages/ResetPassword.tsx
@@ -1,8 +1,39 @@
+import { Box, Button, Flex, FormControl, FormLabel, Heading, Input, Stack, Text } from "@chakra-ui/react";
 
 export function ResetPassword(){
     return(
-        <h1>
-            Reset Password Page
-        </h1>
+        <Flex
+            align={'center'}
+            justify={'center'}
+            minH={'100vh'}
+            bg={'gray.200'}
+        >
+            <Stack spacing={8} maxW={'lg'} py={12} px={6}>
+                <Stack align={'center'} >
+                    <Heading fontSize={'3xl'} textAlign={'center'}>
+                        Redefina sua senha
+                    </Heading>
+                    <Stack>
+                        <Text fontSize={'lg'}>Uma nova senha será enviada para o e-mail informado</Text>
+                    </Stack>
+                </Stack>
+                <Box p={8} rounded={'lg'} boxShadow={'lg'} bg={'white'}>
+                    <Stack spacing={4}>
+                        <FormControl>
+                            <FormLabel id="email">E-mail</FormLabel>
+                            <Input type="email"/>
+                        </FormControl>
+                    </Stack>
+                    <Stack spacing={8}>
+                        <Button
+                            bg={'blue.500'}
+                            _hover={{bg:'gray.400'}}
+                        >
+                            Redefinir senha
+                        </Button>
+                    </Stack>
+                </Box>
+            </Stack>
+        </Flex>
     )
 }
