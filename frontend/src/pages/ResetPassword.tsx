9a7efb068a0685600e273f6eaa4367a74b0655f1
claudiobzrr <claudio.bezerra1998@gmail.com>
2023-04-06 10:46:11 -0300

    Fix: back button position

--- frontend/src/pages/ResetPassword.tsx
+++ frontend/src/pages/ResetPassword.tsx
@@ -1,7 +1,13 @@
 import { Box, Button, Flex, FormControl, FormLabel, Heading, IconButton, Input, Stack, Text } from "@chakra-ui/react";
 import { ArrowBackIcon } from '@chakra-ui/icons'
+import { useNavigate } from "react-router-dom";
 
 export function ResetPassword(){
+    const navigate =  useNavigate()
+
+    function handleGoBack(){
+        navigate(-1)
+    }
     return(
         <Flex
             align={'center'}
@@ -11,9 +17,18 @@ export function ResetPassword(){
         >
             <Stack spacing={8} maxW={'lg'} py={12} px={6}>
                 <Stack align={'center'}>
-                    <Heading fontSize={'3xl'} textAlign={'center'}>
-                        Redefina sua senha
-                    </Heading>
+                    <Stack direction={'row'} justify={'space-between'} align={'center'} w={'100%'}>
+                        <IconButton
+                            bg={'gray.200'}
+                            onClick={handleGoBack}
+                            fontSize={32} 
+                            aria-label="voltar" 
+                            icon={<ArrowBackIcon/>}
+                        />
+                        <Heading fontSize={'3xl'} textAlign={'center'} w={'100%'} pr={'40px'}>
+                            Redefina sua senha
+                        </Heading>
+                    </Stack>
                     <Stack>
                         <Text fontSize={'lg'}>Uma nova senha será enviada para o e-mail informado</Text>
                     </Stack>
