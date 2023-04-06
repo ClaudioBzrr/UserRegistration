2bb227f2f17614481bf7b02a251569c534f1ce19
claudiobzrr <claudio.bezerra1998@gmail.com>
2023-04-06 13:52:09 -0300

    ✔ Added : show password function and icon

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,7 +1,10 @@
-import { Box, Button, Flex, FormControl, FormLabel, Heading, Input, Link, Stack, Text } from "@chakra-ui/react";
+import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
+import { Box, Button, Flex, FormControl, FormLabel, Heading, Input, InputGroup, InputRightAddon, InputRightElement, Link, Stack, Text } from "@chakra-ui/react";
+import { useState } from "react";
 import { Link as Linker } from "react-router-dom";
 
 export function Register(){
+    const [showPassword,setShowPassword] = useState<boolean>(false)
     return(
         <Flex bg={'gray.200'} align={'center'} justify={'center'} minH={'100vh'}>
             <Stack spacing={8}  maxW={'lg'} px={6} py={12}>
@@ -23,7 +26,15 @@ export function Register(){
                         </FormControl>
                         <FormControl>
                             <FormLabel fontSize={'lg'} id="password">Senha</FormLabel>
-                            <Input borderColor={'gray.400'} type="password"/>
+                            <InputGroup>
+                                <Input borderColor={'gray.400'} type={showPassword ==true ? 'text':'password'}/>
+                                <InputRightElement
+                                    onClick={() =>setShowPassword(!showPassword)}
+                                    children={
+                                        showPassword == true?<ViewIcon/>:<ViewOffIcon/>
+                                    }
+                                />
+                            </InputGroup>
                         </FormControl>
                         <Stack spacing={6} pt={6}>
                             <Button 
