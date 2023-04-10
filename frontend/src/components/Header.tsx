d62a6a4a8d9c5ecdb56732f463a7e06c5c153c9b
claudiobzrr <claudio.bezerra1998@gmail.com>
2023-04-09 22:57:34 -0300

    ✔ Added : Dark/Light mode toggler button

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,12 +1,18 @@
-import { Box, Flex, HStack } from "@chakra-ui/react";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import { Box, Flex, HStack, IconButton, Stack, useColorMode,useColorModeValue } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
 
 export function Header(){
+
+    const {colorMode,toggleColorMode} = useColorMode()
+
     return(
-        <Box bg={'white'} position={"fixed"} top={0}>
-            <Flex boxShadow={'lg'} minW={'100vw'} h={16} justify={'space-between'} align={'center'}>
-                    
+        <Box bg={useColorModeValue('white','gray.800')} position={"fixed"} top={0}>
+            <Flex boxShadow={'lg'} minW={'100vw'} h={16} justify={'flex-end'} align={'center'}>
+                <Stack spacing={4} pr={5}>
+                    <IconButton onClick={toggleColorMode} aria-label="toggle color mode" icon={colorMode =='light'? <MoonIcon/> : <SunIcon/>}/>
+                </Stack>
             </Flex>
         </Box>
     )
