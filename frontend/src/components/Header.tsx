cb2a95359bb54f52aba8859a59d2880bcbe614ea
claudiobzrr <claudio.bezerra1998@gmail.com>
2023-04-09 22:32:21 -0300

    ✔ Added: Header box shadow

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,13 +1,12 @@
 import { Box, Flex, HStack } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 
 export function Header(){
     return(
         <Box bg={'white'} position={"fixed"} top={0}>
-            <Flex minW={'100vw'} h={16} justify={'space-between'} align={'center'}>
-                <HStack>
-
-                </HStack>
+            <Flex boxShadow={'lg'} minW={'100vw'} h={16} justify={'space-between'} align={'center'}>
+                    
             </Flex>
         </Box>
     )
