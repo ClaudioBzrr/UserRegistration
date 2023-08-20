aceea77f1c7337ed1a5bd12017ea041c1dbe6dbb
claudiobzrr <claudio.bezerra1998@gmail.com>
2023-08-20 20:48:12 -0300

    ✔ Created: login use-case

---
+++ backend/src/use-cases/login-user-use-case.ts
@@ -0,0 +1,24 @@
+import { IUser } from '../entities/User';
+import { PasswordRepository } from '../repositories/password-repository';
+import { UserRepository } from '../repositories/user-repository';
+
+type IUserLoginData = Pick<IUser, 'email' | 'password'>;
+
+export class LoginUserUseCase {
+  constructor(
+    private userRepository: UserRepository,
+    private passwordRepository: PasswordRepository,
+  ) {}
+  async exec({ email, password }: IUserLoginData) {
+    const user = await this.userRepository.findOne({ email }).catch(() => {
+      throw new Error('Usuário não encrontrado');
+    });
+    const valid = await this.passwordRepository.compare({
+      password,
+      hash: user.password,
+    });
+    if (!valid) {
+      throw new Error('E-mail/senha incorretos');
+    }
+  }
+}
