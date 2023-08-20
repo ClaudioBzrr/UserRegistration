a6d6eaeeb42089017ff4a7cff57d732a46a60681
claudiobzrr <claudio.bezerra1998@gmail.com>
2023-08-20 20:53:31 -0300

    ✔ Created: filter admins use-case

---
+++ backend/src/use-cases/filter-admins-use-case.ts
@@ -0,0 +1,12 @@
+import { IUserType } from '../entities/User';
+import { UserRepository } from '../repositories/user-repository';
+
+export class FilterAdminsUseCase {
+  constructor(private userRepository: UserRepository) {}
+  async exec() {
+    const admins = await this.userRepository.findMany({
+      type: IUserType.ADMIN,
+    });
+    return admins;
+  }
+}
