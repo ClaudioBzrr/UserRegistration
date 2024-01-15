3b1dd49efad0cc2c0182d079639476f761c7c012
claudiobzrr <claudio.bezerra1998@gmail.com>
2024-01-15 20:46:51 -0300

    ✔ Added: cors

--- node+react/backend/src/server.ts
+++ node+react/backend/src/server.ts
@@ -1,8 +1,14 @@
 import 'module-alias/register';
 import 'dotenv/config';
-import Express from 'express';
+import cors from 'cors';
+import express from 'express';
+import { routes } from './routes';
 
-const server = Express();
 const port = String(process.env.SERVER_PORT) || String(process.env.PORT);
+const server = express();
+
+server.use(cors());
+server.use(express.json());
+server.use(routes);
 
 server.listen(port, () => console.log(`Server running on port ${port}`));
--- node+react/frontend/src/services/api.ts
+++
@@ -1,5 +0,0 @@
-import Axios from 'axios'
-
-export const api = Axios.create({
-    baseURL:String(import.meta.env.VITE_API_URL)
-})
