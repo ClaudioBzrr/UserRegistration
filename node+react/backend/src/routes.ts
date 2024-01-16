aacd985287a558e64563d6c2da5d1ea7525be1ca
claudiobzrr <39713988+ClaudioBzrr@users.noreply.github.com>
2024-01-16 16:00:25 +0000

    ✔️ Added : create user controller

--- node+react/backend/src/routes.ts
+++ node+react/backend/src/routes.ts
@@ -6,6 +6,7 @@ const userController = new UserController();
 export const routes = Router();
 
 routes.get('/login', userController.login);
+routes.post('/user', userController.create);
 routes.get('/users', userController.getMany);
 routes.get('/user/:id', userController.getOne);
 routes.put('/user/:id', userController.update);
